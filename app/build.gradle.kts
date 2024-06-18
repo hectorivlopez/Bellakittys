6c2f04cd3716515775a992891395fbf9519cc619
Darypooh29 <dchavira290@gmail.com>
2024-06-17 22:26:48 -0600

    Profile photo able to change with phone multimedia added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Camera dependencies
+    implementation("androidx.camera:camera-core:1.4.0-alpha04")
+    implementation("androidx.camera:camera-camera2:1.4.0-alpha04")
+    implementation("androidx.camera:camera-lifecycle:1.4.0-alpha04")
+    implementation("androidx.camera:camera-video:1.4.0-alpha04")
+    implementation("androidx.camera:camera-view:1.4.0-alpha04")
+    implementation("androidx.camera:camera-extensions:1.4.0-alpha04")
 }
--- app/src/main/java/com/movil/bellakkitys/ProfileActivity.kt
+++ app/src/main/java/com/movil/bellakkitys/ProfileActivity.kt
@@ -1,12 +1,19 @@
 package com.movil.bellakkitys
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import android.Manifest
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 
 class ProfileActivity : AppCompatActivity() {
@@ -29,6 +36,55 @@ class ProfileActivity : AppCompatActivity() {
         imgBtnBack.setOnClickListener { back() }
         logoutBtn.setOnClickListener { logout() }
 
+
+        profileImg.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_CODE_PERMISSIONS
+                )
+            } else {
+                openGalleryForImage()
+            }
+        }
+    }
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == Activity.RESULT_OK) {
+            val selectedImageUri: Uri? = data?.data
+            profileImg.setImageURI(selectedImageUri)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGalleryForImage()
+            } else {
+                println("Permiso denegado")
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 101
+        private const val REQUEST_CODE_SELECT_IMAGE = 100
     }
 
     private fun logout(){
